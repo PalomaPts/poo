54f819a4ce7c501e484dc674e732d757a420ca22
PalomaPts <141692596+PalomaPts@users.noreply.github.com>
2023-08-28 20:31:17 -0300

    gitgnore

---
+++ desafios_lp/src/br/com/poo/lista1/Exercicio1.java
@@ -0,0 +1,33 @@
+package br.com.poo.lista1;
+
+import java.util.Scanner;
+
+//programa do portugol
+
+public class Exercicio1 {
+	
+	//funcao inicio do portugol
+    //funcao no portugol -> metodo no java
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (system.in);
+//      escreva ("Por favor, informe seu nome:")
+//      leia(nome)
+//      limpa()
+//		escreva("Agora, informe seu sobrenome")
+//      leia (sobrenome)
+//      limpa()
+
+		System.out.println("Por favor, informe seu nome:");
+
+		System.out.println("Por favor, informe seu sobrenome:");
+		for(int i=0; i<20; i++) {
+			System.out.println("\n");
+			
+		}
+		    System.out.println("Agora, informe seu sobrenome");
+		}
+		
+
+	}
+
+}
